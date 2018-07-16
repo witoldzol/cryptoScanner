0376babf86872a8bfd6ab7e34d39493a587e9dba
witoldzol <blue3656@gmail.com>
2018-07-16 21:17:31 +0100

    plan out graph api -- not working

---
+++ algo.js
@@ -0,0 +1,39 @@
+// ============================================================ ALGO ==============================
+
+/*
+-
+-build a graph using best edges
+-traverse the graph and plot optimal way (highest prosfit)
+
+
+
+makes a list of all acquired currencies (remove duplis)
+push list to the queue/stack
+pick one from top and 
+
+
+API
+--------------------
+Digraph (int V) - create  empty digraph with V vertices
+
+addEdge(int v, int w) - add a directed ecge v->w
+
+adj(int v) vertices pointing from v
+
+int V - number of vertices
+
+int E - number of edges
+
+Digraph reverse() - reverse of this digraph
+
+String toString - string representation
+
+// PRINT OUT EACH edge once
+forEach( v in G.V )
+    forEach( w : G.adj(v) )
+       console.log( v + "=>" + w )
+
+
+
+
+*/
