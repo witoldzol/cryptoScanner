b6e8e5bf169afe121ba233bea8f7c15d0c1ea5bd
Witold Zolnowski <blue3656@gmail.com>
2020-07-26 16:52:19 +0100

    migrate binance from js to ts

--- built/sample.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = require("../src/util");
-var myvar = 'wow much awesome';
-var mynum = util_1.myExportedFun();
--- src/binance.ts
+++ src/binance.ts
@@ -1,20 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.options = void 0;
-var util = require("./util");
-var pairsUnfiltered = ['ETHBTC', 'LTCBTC'];
+import util = require('./util')
+import { MarketOptions } from './MarketOptions'
+
+let pairsUnfiltered: string[] = ['ETHBTC', 'LTCBTC']
 // let pairsUnfiltered: string[] = ['ETHBTC','LTCBTC','BNBBTC','NEOBTC','QTUMETH','EOSETH','SNTETH','BNTETH','BCCBTC','GASBTC','BNBETH','BTCUSDT','ETHUSDT','HSRBTC','OAXETH','DNTETH','MCOETH','ICNETH','MCOBTC','WTCBTC','WTCETH','LRCBTC','LRCETH','QTUMBTC','YOYOBTC','OMGBTC','OMGETH','ZRXBTC','ZRXETH','STRATBTC','STRATETH','SNGLSBTC','SNGLSETH','BQXBTC','BQXETH','KNCBTC','KNCETH','FUNBTC','FUNETH','SNMBTC','SNMETH','NEOETH','IOTABTC','IOTAETH','LINKBTC','LINKETH','XVGBTC','XVGETH','SALTBTC','SALTETH','MDABTC','MDAETH','MTLBTC','MTLETH','SUBBTC','SUBETH','EOSBTC','SNTBTC','ETCETH','ETCBTC','MTHBTC','MTHETH','ENGBTC','ENGETH','DNTBTC','ZECBTC','ZECETH','BNTBTC','ASTBTC','ASTETH','DASHBTC']
-var pairs = pairsUnfiltered.filter(function (ele) { return ele.length == 6; });
-function formatData(data) {
-    var combinedData = util.mapDataToObject(data);
-    return util.wrapDataInObjectWithMarketName(combinedData, this.marketName);
+let pairs: string[] = pairsUnfiltered.filter(ele => ele.length == 6)
+
+function formatData(data): unknown {
+	let combinedData = util.mapDataToObject(data)
+	return util.wrapDataInObjectWithMarketName(combinedData, this.marketName)
 }
-var options = {
-    marketName: 'BINANCE',
-    baseURL: 'https://api.binance.com/api/v1',
-    urlPath: ['/depth?limit=10&symbol=', ''],
-    pairs: pairs,
-    maxConcurrentRequests: 20,
-    formatData: formatData,
-};
-exports.options = options;
+
+const options: MarketOptions =
+{
+	marketName: 'BINANCE',
+	baseURL: 'https://api.binance.com/api/v1',
+	urlPath: ['/depth?limit=10&symbol=', ''],
+	pairs: pairs,
+	maxConcurrentRequests: 20,
+	formatData: formatData,
+}
+
+export { options } 
