4af438f84e7e405549cd12597c6792758792546b
Witold Zolnowski <blue3656@gmail.com>
2020-07-26 06:36:59 +0100

    update binance

--- binance.js
+++ binance.js
@@ -1,14 +1,12 @@
+const util = require('./util')
+
 let pairsUnfiltered = ['ETHBTC', 'LTCBTC']
 // let pairsUnfiltered = ['ETHBTC','LTCBTC','BNBBTC','NEOBTC','QTUMETH','EOSETH','SNTETH','BNTETH','BCCBTC','GASBTC','BNBETH','BTCUSDT','ETHUSDT','HSRBTC','OAXETH','DNTETH','MCOETH','ICNETH','MCOBTC','WTCBTC','WTCETH','LRCBTC','LRCETH','QTUMBTC','YOYOBTC','OMGBTC','OMGETH','ZRXBTC','ZRXETH','STRATBTC','STRATETH','SNGLSBTC','SNGLSETH','BQXBTC','BQXETH','KNCBTC','KNCETH','FUNBTC','FUNETH','SNMBTC','SNMETH','NEOETH','IOTABTC','IOTAETH','LINKBTC','LINKETH','XVGBTC','XVGETH','SALTBTC','SALTETH','MDABTC','MDAETH','MTLBTC','MTLETH','SUBBTC','SUBETH','EOSBTC','SNTBTC','ETCETH','ETCBTC','MTHBTC','MTHETH','ENGBTC','ENGETH','DNTBTC','ZECBTC','ZECETH','BNTBTC','ASTBTC','ASTETH','DASHBTC']
 let pairs = pairsUnfiltered.filter(ele => ele.length == 6)
 
 function formatData(data){
-	let combineObjects = (arr, obj) => arr.map(x => Object.assign(obj, x))
-	let obj = {}
-	let a = {}
-	combineObjects(data, obj)
-	a[this.marketName] = obj
-	return a
+	let combinedData = util.mapDataToObject(data)
+	return util.wrapDataInObjectWithMarketName(combinedData, this.marketName)
 }
 
 exports.options =
