2d5c6efcb0afc8e2155cc50a3f19ed2ec0177ac9
Witold Zolnowski <blue3656@gmail.com>
2020-09-11 20:22:37 +0100

    convert-main-to-typescript

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { lunoOptions } from "./luno";
+import { binanceOptions } from "./binance";
+import { gdaxOptions } from "./gdax";
+import { getPrices } from "./market-service";
+import { mapDataToObject } from "./util";
+import { GraphService } from "./graph-service";
+
+let graphService = new GraphService();
+
+let lunoPrices = getPrices(lunoOptions);
+let gdaxPrices = getPrices(gdaxOptions);
+let binancePrices = getPrices(binanceOptions);
+
+Promise.all([lunoPrices, gdaxPrices, binancePrices])
+  .then((data) => mapDataToObject(data))
+  .then((data) => {
+    console.log(" MAIN \n" + JSON.stringify(data));
+    return data;
+  })
+  .then((data) => graphService.populateGraph(data))
+  .then((graph) => graph.findNegativeCycles())
+  .catch((e) => console.log("error from main pricess ALL.Promise: " + e.stack));
---
+++ src/models/EdgeValues.ts
@@ -0,0 +1,7 @@
+export interface EdgeValues {
+  priceToLogAndNegative: number;
+  priceWithFees: number;
+  volume: number;
+  marketName: string;
+  askOrBid: string;
+}
